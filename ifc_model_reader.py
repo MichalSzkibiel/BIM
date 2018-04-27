7a7a7ecf3b4b3419728d1f2cd12454cdad561da4
MichalSzkibiel <trolejbusnn@gmail.com>
2018-04-27 11:12:51 +0200

    Update
    Otwiera już strony dla klas

--- ifc_model_reader.py
+++ ifc_model_reader.py
@@ -4,13 +4,18 @@ def read_schemas():
         ifc_dict = {}
         link = r"http://www.steptools.com/stds/ifc/html/"
         fp = urllib.request.urlopen(link + "schema.html")
-        mybytes = fp.read()
-        mystr = mybytes.decode("utf8")
+        html_bytes = fp.read()
+        html_str = html_bytes.decode("utf8")
         fp.close()
-        array = mystr.split(";")
-        for el in array:
+        html_schema = html_str.split(";")
+        for el in html_schema:
             el = el.strip()
-            if (el[:4] == "TYPE"):
-                first_split = el.split(">")
-                second_split = first_split[1].split("<")
-                print(second_split[0])
+            if (el[:4] == "TYPE" or el[:6] == "ENTITY"):
+                    class_link = el.split(r"'")[1]
+                    fp = urllib.request.urlopen(link + class_link)
+                    html_bytes = fp.read()
+                    html_str = html_bytes.decode("utf8")
+                    fp.close()
+                    html_class = html_str.split("*)")
+        
+				
