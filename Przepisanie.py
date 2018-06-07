772eca56544019cfd48dab2f1b5847999529ab54
karoluss96 <33833354+karoluss96@users.noreply.github.com>
2018-06-07 16:14:32 +0200

    zmiany wczoraj

--- Przepisanie.py
+++ Przepisanie.py
@@ -2,12 +2,7 @@ import psycopg2
 from subprocess import call
 
 
-#tab1=["Foreign_key_list","Value"]
-#tab2=["Integer_list","Value"]
-#tab3=["Real_list","Value"]
-#tab4=["String_list","Value"]
-ifc_name1={"nazwa_typu" : "INTEGER", "nazwa_entity":{"parent":"nazwa_parent", "attribute_list":[{"name":"nazwa_typu"},{"ifc_class":[{"set":[{"klucz":"INTEGER"},{"TYPE":"Tip_a_Type"},{"ENTITY":"Foreign_key_list"}]},
-"NO_set":[{"TYPE":"tip_a_Type"},{"ENTITY":"Foreign_key_String"}]]}], "is_in_database":False}, "nazwa_enum" : ["string1", "string2"]}
+ifc_name1={"nazwa_typu" : "INTEGER", "nazwa_entity":{"parent":"nazwa_parent", "attribute_list":{"name":"nazwa_typu", "ifc_class":"nazwa_entity"}, "is_in_table":False}, "nazwa_enum" : ["string1","string2"]}
 for element in ifc_name1:
     if type(ifc_name1[element]) == type("string"):#sprawdzamy typy
         continue
@@ -20,7 +15,7 @@ for element in ifc_name1:
         conn.commit()
         cur.close()
         conn.close()    
-    else
+    else:
        if not (ifc_name1[ifc_name1[element]["parent"]]["is_in_table"]):#I) Sprawdzamy, czy rodzic jest odznaczony
            insert_entity(ifc_name1,element)
            #ciag dalszy w funkcji insert_entity
--- insert_entity.py
+++ insert_entity.py
@@ -1,20 +1,26 @@
 # -*- coding: cp1250 -*-
-def insert_entity(ifc_name,element):
+def insert_entity(ifc_dict, ifc_name,element):
     if ifc_name1[[element]["is_in_table"]]:
         return
     if not (ifc_name1[ifc_name1[element]["parent"]]["is_in_table"]):
         insert_entity(ifc_name,ifc_name1[element]["parent"]) #1. Ustawiamy obecna… klasa na rodzica
-        conn = psycopg2.connect("dbname=postgres user=Karol Jakub")
-        cur = conn.cursor()
-        cur.execute("CREATE TABLE entity(id enumeration PRIMARY KEY, ifc_name1 bigint);")
-        for i in ifc_name1[element]:
-            cur.execute("INSERT INTO entity  VALUES (%s)",i)#2. Robimy procedure pod 2.a.i: Zaladowanie strony klasy
-        conn.commit()
-        cur.close()
-        conn.close()
-        conn1 = psycopg2.connect("dbname=postgres user=Karol Jakub")
-        cur1 = conn1.cursor()
-        cur1.execute("CREATE TABLE class(id serial PRIMARY KEY, ifc_name1 bigint);")#3. Wracamy do obecnej klasy
-        cur1.execute(SQL("INSERT INTO {} VALUES (%s)").format(Identifier(ifc_name1)),i)#II Wstawiamy tabelę dla danej klasy
-        "is_in_table"=True#III Odznaczamy klasę		
+    conn = psycopg2.connect("dbname=postgres user=Karol Jakub")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE %s(id enumeration PRIMARY KEY, (%s) bigint );")#jedna tabela dla kazdego entity i jedna kolumna dla kazdego atrybutu
+    for i in ifc_name1[element][attributes]:# i to klucz a pod wartoscia jest tryb
+		#2. Robimy procedure pod 2.a.i: Zaladowanie strony klasy
+        ifc_name= ifc_dict(ifc_name1[element][attributes][i])
+        if ifc_name!=ifc_name1[[element]["String"]]:
+            ifc_name='TEXT'
+            cur.execute("ALTER TABLE %s ADD %s text;")
+        else:
+            cur.execute("ALTER TABLE %s ADD %s %s", ["ifc_name","attributes","type"]) 
+    conn.commit()
+    cur.close()
+    conn.close()
+    conn1 = psycopg2.connect("dbname=postgres user=Karol Jakub")
+    cur1 = conn1.cursor()
+    cur1.execute("CREATE TABLE %s(id serial PRIMARY KEY, %s bigint);")#3. Wracamy do obecnej klasy
+    cur1.execute(SQL("INSERT INTO %s VALUES (%s)").format(Identifier(ifc_name1)),i)#II Wstawiamy tabelę dla danej klasy
+    ifc_name1[[element]["is_in_table"]]=True#III Odznaczamy klasę		
 	#c) Ustawienie kluczy obcych
