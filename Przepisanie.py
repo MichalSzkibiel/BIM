094da1e20d83b115e5d517a53f65f22d9f220f1a
karoluss96 <33833354+karoluss96@users.noreply.github.com>
2018-06-11 16:56:06 +0200

    Poprawienie funkcji

--- Przepisanie.py
+++ Przepisanie.py
@@ -2,16 +2,16 @@ import psycopg2
 from subprocess import call
 from insert_entity import insert_entity
 
-ifc_name1={"nazwa_typu" : "INTEGER", "nazwa_entity":{"parent":"nazwa_parent", "attribute_list":{"name":"nazwa_typu", "ifc_class":"nazwa_entity"}, "is_in_table":False}, "nazwa_enum" : ["string1","string2"]}
+ifc_name1={"nazwa_typu" : "INTEGER", "nazwa_entity":{"parent":None, "attributes":{"name":"nazwa_typu", "ifc_class":"nazwa_entity"}, "is_in_table":False}, "nazwa_enum" : ["string1","string2"]}
 for element in ifc_name1:
     if type(ifc_name1[element]) == type("string"):#sprawdzamy typy
         continue
     elif type(ifc_name1[element]) == type([]):
-        conn = psycopg2.connect("dbname=postgres user=Karol Jakub")
+        conn = psycopg2.connect("dbname=postgres user=postgres password=Qjr567*_+")
         cur = conn.cursor()
-        cur.execute("CREATE TABLE element(id text PRIMARY KEY);")
+        cur.execute("CREATE TABLE " + element + " (id text PRIMARY KEY);")
         for i in ifc_name1[element]:
-            cur.execute("INSERT INTO element  VALUES (%s)",i)
+            cur.execute("INSERT INTO " + element + "  VALUES (%s)", [i])
         conn.commit()
         cur.close()
         conn.close()    
--- insert_entity.py
+++ insert_entity.py
@@ -1,25 +1,31 @@
 # -*- coding: cp1250 -*-
+import psycopg2
+from subprocess import call
 def insert_entity(ifc_name,element):
-    if ifc_name[[element]["is_in_table"]]:
+    if ifc_name[element]["is_in_table"]:
         return
-    if "parent" in ifc_name[element]:
-            if not (ifc_name[ifc_name[element]["parent"]]["is_in_table"]):#I) Sprawdzamy, czy rodzic jest odznaczony
-                 insert_entity(ifc_name,ifc_name[element]["parent"])
-    conn = psycopg2.connect("dbname=postgres user=Karol Jakub")
+    conn = psycopg2.connect("dbname=postgres user=postgres password=Qjr567*_+")
     cur = conn.cursor()
-    cur.execute("CREATE TABLE %s INHERITS (%s) WITH (OIDS=TRUE) );", [ifc_name,element,ifc_name[ifc_name[element]["parent"]]])#jedna tabela dla kazdego entity i jedna kolumna dla kazdego atrybutu
-    for i in ifc_name[element][attributes]:# i to klucz a pod wartoscia jest tryb
-		#2. Robimy procedure pod 2.a.i: Zaladowanie strony klasy
-        ifc_name1= ifc_name[ifc_name[element][attributes][i]]
-        if ifc_name1!=ifc_name[[element]["String"]]:
-            ifc_name1='TEXT'
-            cur.execute("ALTER TABLE %s ADD %s text;", [ifc_name, i])
+    if "parent" in ifc_name[element]:
+        if ifc_name[element]["parent"] !=None:
+            if not (ifc_name[ifc_name[element]["parent"]]["is_in_table"]):#I) Sprawdzamy, czy rodzic jest odznaczony
+                insert_entity(ifc_name,ifc_name[element]["parent"])
+            cur.execute("CREATE TABLE IF NOT EXISTS " + element + " INHERITS ( " + ifc_name[ifc_name[element]["parent"]]+ ") WITH (OIDS=TRUE);", [ifc_name,element,ifc_name[ifc_name[element]["parent"]]])
+				#jedna tabela dla kazdego entity i jedna kolumna dla kazdego atrybutu
         else:
-            cur.execute("ALTER TABLE %s ADD %s %s", [ifc_name, i, ifc_name1]) 
+                cur.execute("CREATE TABLE IF NOT EXISTS " + element + "() WITH (OIDS=TRUE)", [element])
+    for i in ifc_name[element]["attributes"]:# i to klucz a pod wartoscia jest tryb
+		#2. Robimy procedure pod 2.a.i: Zaladowanie strony klasy
+        ifc_name1= ifc_name[ifc_name[element]["attributes"][i]]
+        if type(ifc_name1) != type(""):
+            ifc_name1='TEXT'
+            cur.execute("ALTER TABLE " + element + " ADD " + i + " text", [element, i])
+        else:
+            cur.execute("ALTER TABLE " + element + " ADD " + i + " " + ifc_name1, [element, i, ifc_name1]) 
     conn.commit()
     cur.close()
     conn.close()
-    conn1 = psycopg2.connect("dbname=postgres user=postgres")
+    conn1 = psycopg2.connect("dbname=postgres user=postgres password=Qjr567*_+")
     cur1 = conn1.cursor()
-    ifc_name1[[element]["is_in_table"]]=True#III Odznaczamy klasę		
+    ifc_name[element]["is_in_table"]=True#III Odznaczamy klasę		
 	#c) Ustawienie kluczy obcych
