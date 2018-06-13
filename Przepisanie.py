6fa1083f7bdc122ef323802172df63870e75e6a5
karoluss96 <33833354+karoluss96@users.noreply.github.com>
2018-06-13 09:54:35 +0200

    Kolejne poprawki, może już ostatnie?

--- Przepisanie.py
+++ Przepisanie.py
@@ -1,19 +1,27 @@
 import psycopg2
 from subprocess import call
 from insert_entity import insert_entity
+from ifc_dict import ifc_dict
 
-ifc_name1={"nazwa_typu" : "INTEGER", "nazwa_entity":{"parent":None, "attributes":{"name":"nazwa_typu", "ifc_class":"nazwa_entity"}, "is_in_table":False}, "nazwa_enum" : ["string1","string2"]}
+ifc_name1=ifc_dict
 conn = psycopg2.connect("dbname=postgres user=postgres password=Qjr567*_+")
 cur = conn.cursor()
-cur.execute("CREATE TABLE set_real (id text PRIMARY KEY, values real[])")
-cur.execute("CREATE TABLE set_integer (id text PRIMARY KEY, values integer[])")
-cur.execute("CREATE TABLE set_entity (id text PRIMARY KEY, values text[])")
+#cur.execute("CREATE TABLE set_real (id text PRIMARY KEY, values real[]);")
+#cur.execute("CREATE TABLE set_integer (id text PRIMARY KEY, values integer[]);")
+#cur.execute("CREATE TABLE set_entity (id text PRIMARY KEY, values text[]);")
+conn.commit()
+conn.close()
 for element in ifc_name1:
     if type(ifc_name1[element]) == type("string"):#sprawdzamy typy
         continue
     elif type(ifc_name1[element]) == type([]):
         conn = psycopg2.connect("dbname=postgres user=postgres password=Qjr567*_+")
         cur = conn.cursor()
+        conn = psycopg2.connect("dbname=postgres user=postgres password=Qjr567*_+")
+        cur = conn.cursor()
+        cur.execute("select exists(select * from information_schema.tables where table_name=%s)", (element,))
+        if bool(cur.rowcount):
+            continue
         cur.execute("CREATE TABLE " + element + " (id text PRIMARY KEY);")
         for i in ifc_name1[element]:
             cur.execute("INSERT INTO " + element + "  VALUES (%s)", [i])
--- insert_entity.py
+++ insert_entity.py
@@ -4,21 +4,27 @@ from subprocess import call
 def insert_entity(ifc_name,element):
     if ifc_name[element]["is_in_table"]:
         return
+    print (element)
     conn = psycopg2.connect("dbname=postgres user=postgres password=Qjr567*_+")
     cur = conn.cursor()
     if "parent" in ifc_name[element]:
         if ifc_name[element]["parent"] !=None:
             if not (ifc_name[ifc_name[element]["parent"]]["is_in_table"]):#I) Sprawdzamy, czy rodzic jest odznaczony
                 insert_entity(ifc_name,ifc_name[element]["parent"])
-            cur.execute("CREATE TABLE IF NOT EXISTS " + element + " INHERITS ( " + ifc_name[ifc_name[element]["parent"]]+ ") WITH (OIDS=TRUE);", [ifc_name,element,ifc_name[ifc_name[element]["parent"]]])
+            cur.execute("CREATE TABLE IF NOT EXISTS " + element + "() INHERITS ( " + ifc_name[element]["parent"]+ ") WITH (OIDS=TRUE);")
 				#jedna tabela dla kazdego entity i jedna kolumna dla kazdego atrybutu
         else:
                 cur.execute("CREATE TABLE IF NOT EXISTS " + element + "() WITH (OIDS=TRUE)", [element])
     for i in ifc_name[element]["attributes"]:# i to klucz a pod wartoscia jest tryb
 		#2. Robimy procedure pod 2.a.i: Zaladowanie strony klasy
         ifc_name1= ifc_name[element]["attributes"][i]
+        print(i)
+        print(ifc_name1)
         if not "SET" in ifc_name1:
-            ifc_name1 = ifc_name[ifc_name1]  
+            if ifc_name1 in ifc_name:
+                ifc_name1 = ifc_name[ifc_name1]
+            else:
+                ifc_name1 = "TEXT"
         elif not "SET_SET" in ifc_name1:
             ifc_name1 = ifc_name[ifc_name1.replace("SET_", "")]
             if type(ifc_name1) != type(""):
@@ -29,7 +35,10 @@ def insert_entity(ifc_name,element):
             ifc_name1 = "TEXT"
         if type(ifc_name1) != type(""):
             ifc_name1='TEXT'
-        cur.execute("ALTER TABLE " + element + " ADD " + i + " " + ifc_name1)
+        try:
+            cur.execute("ALTER TABLE " + element + " ADD " + i + " " + ifc_name1)
+        except:
+            print("Nie uda³o siê wstawiæ")
         
     conn.commit()
     cur.close()
